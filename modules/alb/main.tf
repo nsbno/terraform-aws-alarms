d798d8b08f349857ef757508e144646c119577e5
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-02-04 15:11:41 +0100

    Add alarms for Application Load Balancers

---
+++ modules/alb/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "service_health" {
+  alarm_name        = "${var.name_prefix}-unhealthy"
+  alarm_description = "${var.name_prefix} has ${var.healthy_host_threshold} or less healthy hosts"
+
+  metric_name = "HealthyHostCount"
+  namespace   = "AWS/ApplicationELB"
+  dimensions  = {
+    TargetGroup  = var.target_group_arn_suffix
+    LoadBalancer = var.load_balancer_arn_suffix
+  }
+
+  statistic           = "Sum"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  threshold           = var.healthy_host_threshold
+  period              = 60
+  evaluation_periods  = var.healthy_hosts_times_failed
+
+  alarm_actions = var.alarm_sns_topic_arns
+  ok_actions    = var.alarm_sns_topic_arns
+}
+
---
+++ modules/alb/variables.tf
@@ -0,0 +1,36 @@
+variable "name_prefix" {
+  description = "Prefix to apply to the alarm names"
+  type = string
+}
+
+variable "target_group_arn_suffix" {
+  description = "Target Group to monitor"
+  type = string
+}
+
+variable "load_balancer_arn_suffix" {
+  description = "Load Balancer to monitor"
+  type = string
+}
+
+variable "alarm_sns_topic_arns" {
+  description = "Where to send Alarms and OKs"
+  type = list(string)
+}
+
+/*
+ * == Alarm Configuration
+ */
+variable "healthy_host_threshold" {
+  description = "The amount of healthy hosts on the ALB before an alarm is triggered"
+  type = number
+
+  default = 0
+}
+
+variable "healthy_hosts_times_failed" {
+  description = "Number of times the healthy hosts test can fail before an alarm is triggered"
+  type = number
+
+  default = 2
+}
