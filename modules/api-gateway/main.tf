9e68ce8dd319c019c57ed1020c9e48e01d56bf4f
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-02-04 14:38:48 +0100

    Add alarms for API Gateway

---
+++ modules/api-gateway/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_latency" {
+  alarm_name        = "${var.name_prefix}-${var.api_name}-latency"
+  alarm_description = "${var.api_name} latency is above configured treshold"
+
+  metric_name = "IntegrationLatency"
+  namespace   = "AWS/ApiGateway"
+  dimensions  = {
+    ApiName = "${var.api_name}"
+  }
+
+  statistic           = "Average"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = var.latency_threshold
+  period              = 60
+  evaluation_periods  = var.latency_times_failed
+  treat_missing_data  = "notBreaching"
+
+  alarm_actions = var.alarm_sns_topic_arns
+  ok_actions    = var.alarm_sns_topic_arns
+}
+
+resource "aws_cloudwatch_metric_alarm" "num_errors_service" {
+  alarm_name        = "${var.name_prefix}-${var.api_name}-5xx-errors"
+  alarm_description = "${var.api_name} has crossed the 5xx error treshold"
+
+  metric_name = "5XXError"
+  namespace   = "AWS/ApiGateway"
+  dimensions  = {
+    ApiName = "${var.api_name}"
+  }
+
+  statistic           = "Average"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = var.error_5xx_threshold
+  period              = 60
+  evaluation_periods  = var.error_5xx_times_failed
+  treat_missing_data  = "notBreaching"
+
+  alarm_actions = var.alarm_sns_topic_arns
+  ok_actions    = var.alarm_sns_topic_arns
+}
+
---
+++ modules/api-gateway/variables.tf
@@ -0,0 +1,45 @@
+variable "name_prefix" {
+  description = "Prefix to apply to the alarm names"
+  type = string
+}
+
+variable "api_name" {
+  description = "The name of the API in API Gateway"
+  type = string
+}
+
+variable "alarm_sns_topic_arns" {
+  description = "Where to send Alarms and OKs"
+  type = list(string)
+}
+
+/*
+ * == Alarm Configuration
+ */
+variable "latency_threshold" {
+  description = "The amount of milliseconds of latency the application must have before an alarm is triggered"
+  type = number
+
+  default = 1000
+}
+
+variable "latency_times_failed" {
+  description = "The amount of evaluation periods that should elapse before an alarm is triggered"
+  type = number
+
+  default = 5
+}
+
+variable "error_5xx_threshold" {
+  description = "The amount of 5XX errors before an alarm is triggered"
+  type = number
+
+  default = 50
+}
+
+variable "error_5xx_times_failed" {
+  description = "The amount of evaluation periods that should elapse before an alarm is triggered"
+  type = number
+
+  default = 3
+}
