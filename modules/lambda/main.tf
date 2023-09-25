236eda564707d0a70c30b7dae0847390ed23b65d
Wictor Zhao <wictor_x@hotmail.com>
2023-09-25 14:19:34 +0200

    Add alarm for Lambda

---
+++ modules/lambda/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  alarm_name        = "${var.name_prefix}-${var.lambda_name}-error"
+  alarm_description = "${var.lambda_name} has had errors above configured threshold"
+
+  metric_name = "Errors"
+  namespace   = "AWS/Lambda"
+
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = var.error_threshold
+  period              = var.period
+  evaluation_periods  = var.times_failed
+  treat_missing_data  = var.treat_missing_data
+
+  dimensions = {
+    FunctionName = var.lambda_name
+  }
+
+  alarm_actions = var.alarm_sns_topic_arns
+  ok_actions    = var.alarm_sns_topic_arns
+}
+
---
+++ modules/lambda/variables.tf
@@ -0,0 +1,43 @@
+variable "name_prefix" {
+  description = "Prefix to apply to the alarm names"
+  type        = string
+}
+
+variable "lambda_name" {
+  description = "The name of the Lambda"
+  type        = string
+}
+
+variable "alarm_sns_topic_arns" {
+  description = "Where to send Alarms and OKs"
+  type        = list(string)
+}
+
+/*
+ * == Alarm Configuration
+ */
+
+variable "error_threshold" {
+  description = "Amount of times the lambda can fail before an alarm is triggered"
+  type        = number
+  default     = 1
+}
+
+variable "times_failed" {
+  description = "Amount of elapsed evaluation periods before an alarm is triggered"
+  type        = number
+  default     = 1
+}
+
+variable "treat_missing_data" {
+  description = "How this alarm is to handle missing data points"
+  type        = string
+  default     = "missing"
+}
+
+
+variable "period" {
+  description = "Period in seconds to check for the applied statistic"
+  type        = number
+  default     = 60
+}
