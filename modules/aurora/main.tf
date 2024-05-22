aaf2873f07fa69c964f7718d93d2cb4ebee1f41f
SondreODahl <sondreo.dahl@gmail.com>
2024-05-22 12:43:59 +0200

    Use SurplusCreditsCharged to see when extra cpu credits are spent

--- modules/aurora/main.tf
+++ modules/aurora/main.tf
@@ -13,18 +13,18 @@ resource "aws_cloudwatch_metric_alarm" "aurora_cpu_credits_empty" {
   alarm_name        = "${var.aurora_db_instance_name}-aurora-cpu-credits-empty"
   alarm_description = "${var.aurora_db_instance_name} has run out of Aurora CPU credits"
 
-  metric_name = "CPUCreditBalance"
+  metric_name = "CPUCreditSurplusCreditsCharged"
   namespace   = "AWS/RDS"
 
   dimensions = {
     DBInstanceIdentifier = var.aurora_db_instance_name
   }
 
-  statistic           = "SampleCount"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  threshold           = 0
-  period              = 60
-  evaluation_periods  = 5
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = var.maximum_possible_cpu_credits * var.percentage_threshold_to_monitor
+  period              = 300
+  evaluation_periods  = 3
 
   alarm_actions = var.alarm_sns_topic_arns
   ok_actions    = var.alarm_sns_topic_arns
--- modules/aurora/variables.tf
+++ modules/aurora/variables.tf
@@ -3,7 +3,20 @@ variable "aurora_db_instance_name" {
   type        = string
 }
 
+variable "maximum_possible_cpu_credits" {
+  description = "The maximum number of CPU credits that can be earned for the Aurora DB Instance." +
+          " Available at https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/burstable-credits-baseline-concepts.html#burstable-performance-instances-credit-table"
+}
+
+variable "percentage_threshold_to_monitor" {
+  description = "The percentage of spent surplus CPU credits to monitor for. The percentage is used to calculate" +
+          " the threshold value by using the max possible cpu credits. Defaults to 60%."
+  default = 0.6
+}
+
 variable "alarm_sns_topic_arns" {
   description = "Where to send Alarms and OKs"
   type        = list(string)
 }
+
+
