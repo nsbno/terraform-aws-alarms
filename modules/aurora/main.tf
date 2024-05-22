b7761ad536cb9dccf0e7027ca44fb07ae55f0d81
SondreODahl <sondreo.dahl@gmail.com>
2024-05-22 13:56:32 +0200

    Fix alarm name and desc

--- modules/aurora/main.tf
+++ modules/aurora/main.tf
@@ -10,8 +10,8 @@ terraform {
 }
 
 resource "aws_cloudwatch_metric_alarm" "aurora_cpu_surplus_credits_charged" {
-  alarm_name        = "${var.aurora_db_instance_name}-aurora-cpu-credits-empty"
-  alarm_description = "${var.aurora_db_instance_name} has run out of Aurora CPU credits"
+  alarm_name        = "${var.aurora_db_instance_name}-aurora_cpu_surplus_credits_charged"
+  alarm_description = "${var.aurora_db_instance_name} has incurred charges for overspent CPU credits."
 
   metric_name = "CPUCreditSurplusCreditsCharged"
   namespace   = "AWS/RDS"
